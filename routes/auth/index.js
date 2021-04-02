da7851fd2780e138ac1ef9e3462865aac7870e6a
lntl <l_natale@hotmail.com>
2021-04-02 13:52:09 +0200

    Add endpoint post request to get jwt for React appli

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -5,6 +5,26 @@ const	axios = require('axios'),
       router = express.Router(),
       jwt_decode = require('jwt-decode');
 
+router.post('/getjwt', function(req, res, next) {
+  axios({
+    method: 'post',
+    url: 'http://127.0.0.1:8000/api/auth/login', 
+    data : qs.stringify({
+      email: req.body.email,
+      password: req.body.password
+    }),
+    headers:{
+      'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+    }
+  }).then(function(response) {
+    var jwt = jwt_decode(response.data.jwt);
+    res.send(jwt);
+  }).catch(function(error) {
+    res.send(false);
+  })
+})
+
+
 router.use(function(req, res, next) {
   axios({
     method: 'post',
